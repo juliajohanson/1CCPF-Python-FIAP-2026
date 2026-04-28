8e4fd1a078eb8158e055a6bd5fa02b42cbc056d5
juliajohansonpeniche <juliajohansonpeniche@gmail.com>
2026-04-28 11:38:17 -0300

    aula 4 estruturas while e for

---
+++ aula04/aula-04a-while.py
@@ -0,0 +1,17 @@
+
+def verificar_nota(nota):
+      while nota < 0 or nota > 10:
+          print("A nota deve estar entre 0 e 10")
+          nota = float(input("digite a nota novamente:"))
+      return nota
+
+notaA = float(input("Digite a 1 nota: "))
+notaA = verificar_nota(notaA)
+
+notaB = float (input("Digite a 2 nota: "))
+while notaB < 0 or notaB > 10:
+    print("A nota deve estar entre 0 e 10")
+    notaB = float(input("digite a nota novamente:"))
+
+media = (notaA + notaB) / 2
+print(media)
---
+++ aula04/aula-04b-for.py
@@ -0,0 +1,12 @@
+# for count_music in range(3):
+#     print(f"Música {count_music}")
+#
+# # de 1 ate 11, pulando de 2 em 2
+# for i in range(1, 12, 2):
+#     print(i)
+# LAÇÕS ALINHADOS
+
+# REP. ENCADEADA
+for i in range(0, 4):
+    for j in range(0, 3):
+        print(f"i:{i}, j:{j}")
